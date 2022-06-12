c602ef3d915e4b5537d4935178966b771a4e8f05
FacundoRago <facundorago98@gmail.com>
2022-06-12 20:30:57 -0300

    [Landing] - Roadmap on mobile
    Yet to finish

--- src/sections/Roadmap/Roadmap.tsx
+++ src/sections/Roadmap/Roadmap.tsx
@@ -1,6 +1,10 @@
 // IMPORT REACT
 import React from "react";
 
+// Import Expandable Component
+import "@blueprintjs/core/lib/css/blueprint.css";
+import { Collapse, Button } from "@blueprintjs/core";
+
 // IMPORT STYLES
 import styles from "./Roadmap.module.css";
 
@@ -9,12 +13,45 @@ type RoadmapProps = {
 };
 
 const Roadmap: React.FC<RoadmapProps> = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   ////////////
   // RENDER //
   ////////////
   return (
     <div className={styles.Container}>
         <h2 className={styles.Title}>ROADMAP</h2>
+
+        <div className={styles.RoadMapContainer}>
+            <div className={styles.Item} style={{ marginTop: "0" }}>
+            <div className={styles.Ball} />
+                <a onClick={() => setIsOpen(!isOpen)} className={styles.Button}>
+                    LOREM
+                    <Collapse isOpen={isOpen}>
+                        <ol className={styles.List}>
+                            <li>
+                                Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                            </li>
+                        </ol>
+                    </Collapse>
+                </a>
+            </div>
+
+            <div className={styles.Item}>
+                <div className={styles.Ball} />
+                <p className={styles.Button}>LOREM</p>
+            </div>
+
+            <div className={styles.Item}>
+                <div className={styles.Ball} />
+                <p className={styles.Button}>LOREM</p>
+            </div>
+
+            <div className={styles.Item} style={{ marginBottom: "0" }}>
+                <div className={styles.Ball} />
+                <p className={styles.Button}>LOREM</p>
+            </div>
+        </div>
     </div>
   );
 };
