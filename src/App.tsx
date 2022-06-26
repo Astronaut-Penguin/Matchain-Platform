62b7bb5b3388bd2936e2fa1604a565a3145b68ef
Leonel Salvo <yupisenpai@hotmail.com>
2022-06-26 18:35:01 -0300

    Changed to render landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './views/Landing/Landing';
 // import Landing from './views/Landing/Landing';
 import Profile from './views/Profile/Profile';
 
 function App() {
-	// return <Landing />;
-	return <Profile />;
+	return <Landing />;
 }
 
 export default App;
