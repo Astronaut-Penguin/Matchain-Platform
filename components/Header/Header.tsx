0e97e503a943b7d4fae9d1f9edef60a1c68c29f2
Leonel Salvo <yupisenpai@hotmail.com>
2022-07-29 21:32:18 -0300

    Inline nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import styles from "./Header.module.css";
 
 // IMPORT BOOTSTRAP
-import { Navbar, Container, Nav, } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 import Image from "next/image";
 
@@ -23,75 +23,89 @@ const Header: React.FC<HeaderProps> = () => {
   ////////////
   return (
     <div className={styles.Container}>
-      <Image src="/logo512.png" width={"50%"} height={"50%"} className={styles.Logo}/>
-      <Image src="/logo512.png" width={"50%"} height={"50%"} className={styles.Profile}/>
-      <Navbar>
-        <Container>
-          <Flicking
-            className="flicking-viewport keywords-flicking mb-4"
-            align="center"
-            circular={false}
-            onMoveEnd={(e) => {
-              console.log(e);
-            }}
-          >
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#home"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                HOME
-              </Nav.Link>
-            </div>
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#fighters"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                GAMES
-              </Nav.Link>
-            </div>
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#education"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                EDUCATION
-              </Nav.Link>
-            </div>
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#roadmap"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                ROADMAP
-              </Nav.Link>
-            </div>
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#about"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                ABOUT US
-              </Nav.Link>
-            </div>
-            <div className={styles.Panel}>
-              <Nav.Link
-                href="#team"
-                className={styles.Element}
-                bsPrefix="Element"
-              >
-                TEAM
-              </Nav.Link>
-            </div>
-          </Flicking>
-        </Container>
-      </Navbar>
+      <div className={styles.ImageContainer}>
+        <Image
+          src="/logo512.png"
+          width={60}
+          height={60}
+          className={styles.Logo}
+        />
+        <Image
+          src="/logo512.png"
+          width={60}
+          height={60}
+          className={styles.Profile}
+        />
+      </div>
+      <div className={styles.NavContainer}>
+        <Navbar className={styles.Nav}>
+          <Container>
+            <Flicking
+              className="flicking-viewport keywords-flicking mb-4"
+              align="center"
+              circular={false}
+              onMoveEnd={(e) => {
+                console.log(e);
+              }}
+            >
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#home"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  HOME
+                </Nav.Link>
+              </div>
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#fighters"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  GAMES
+                </Nav.Link>
+              </div>
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#education"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  EDUCATION
+                </Nav.Link>
+              </div>
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#roadmap"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  ROADMAP
+                </Nav.Link>
+              </div>
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#about"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  ABOUT US
+                </Nav.Link>
+              </div>
+              <div className={styles.Panel}>
+                <Nav.Link
+                  href="#team"
+                  className={styles.Element}
+                  bsPrefix="Element"
+                >
+                  TEAM
+                </Nav.Link>
+              </div>
+            </Flicking>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 };
