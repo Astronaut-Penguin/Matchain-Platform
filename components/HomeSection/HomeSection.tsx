3fc6346c1e35b99e7974b1227b2744033c5b5e2e
Leonel Salvo <yupisenpai@hotmail.com>
2022-07-31 22:47:02 -0300

    Added HomeSection html, pending css

--- components/HomeSection/HomeSection.tsx
+++ components/HomeSection/HomeSection.tsx
@@ -22,7 +22,57 @@ const HomeSection: React.FC<HomeProps> = () => {
   ////////////
   // RENDER //
   ////////////
-  return <></>;
+  return (
+    <div className={styles.Container} id="home">
+      <Swiper
+        pagination={{
+          el: ".my-custom-pagination-div",
+          clickable: true,
+          renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + "" + "</span>";
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className={styles.Slide}>
+          <Image
+            src={"/assets/Home/slide2.png"}
+            layout="fill"
+            className={styles.Image2}
+          />
+          <Image
+            src={"/assets/Home/texture.png"}
+            layout="fill"
+            className={styles.Texture}
+          />
+          <h2 className={styles.Start}>
+            START YOUR <b>OWN GAME</b> NOW
+          </h2>
+          <div className={styles.Learn}>
+            <BlueButton text={"LEARN NOW"} />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.Slide}>
+          <Image
+            src={"/assets/Home/slide1.png"}
+            layout="fill"
+            className={styles.Image}
+          />
+          <h2 className={styles.Title}>WORKAHOLIC</h2>
+          <Image
+            src={"/assets/Home/character.gif"}
+            layout="fill"
+            className={styles.Character}
+          />
+          <div className={styles.Play}>
+            <BlueButton text={"PLAY NOW"} />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default HomeSection;
